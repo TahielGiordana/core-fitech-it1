8f9b0a6500b2aba7bba165a554046feefba5e442
jferdev <fernando.ene@besysoft.com>
2023-04-04 16:40:47 -0300

    se simula consulta a servicios externos para obtener rutinas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
+import org.json.simple.JSONObject;
+import service.RoutineConnectorService;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("HOLA MUNDO");
+        //prueba lectura archivo json, ver si hace falta implementar DTO
+        JSONObject rutina1 = RoutineConnectorService.getRutina1();
+        System.out.println("Consultando rutina de servicios externos");
+        System.out.println("se obtiene: " + rutina1);
     }
 }
--- src/main/java/pojos/Routine.java
+++ src/main/java/pojos/Routine.java
@@ -4,11 +4,22 @@ import java.util.List;
 
 public class Routine {
 
+	private String name;
+
 	private List<String> machineSerialCodes;
 
-    public Routine(List<String> machineSerialCodes) {
-        this.setMachineSerialCodes(machineSerialCodes);
-    }
+	public Routine(String name, List<String> machineSerialCodes) {
+		this.name = name;
+		this.machineSerialCodes = machineSerialCodes;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 	public List<String> getMachineSerialCodes() {
 		return machineSerialCodes;
