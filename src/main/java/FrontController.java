483dc0210935935cf7052f02beb6d705bda6901f
Tony-L-93 <tonyliendro@gmail.com>
2023-04-11 00:51:45 -0300

    Se agrega datos iguales a los del map y se cambia algunos nombres

--- src/main/java/FrontController.java
+++ src/main/java/FrontController.java
@@ -1,23 +1,19 @@
 import pojos.Machine;
 import pojos.Person;
-import pojos.Routine;
 import services.RoutineFinder;
 import validators.RoutineValidator;
-
-import java.util.Arrays;
 
 public class FrontController {
 
     public static void main(String[] args) {
-        Person person = new Person(new Routine("rutina_a", Arrays.asList("A1","A2")), "persona_1", "persona_1");
-        Machine machine = new Machine("A1","maquina_1");
+        Person person = new Person("Joan", "Ene");
+        Machine machine =new Machine("07569LK500IC0122", "Stationary Bike");
         FrontController frontController = new FrontController();
-        boolean validar = frontController.validar(person, machine);
-        System.out.println("Validar: " + validar);
-
+        boolean validateMachine = frontController.validateMachine(person, machine);
+        System.out.println("Uso de Bicicleta Fija: " + validateMachine);
     }
 
-    public boolean validar(Person persona, Machine machine){
+    public boolean validateMachine(Person persona, Machine machine){
         RoutineValidator routineValidator = new RoutineValidator(new RoutineFinder());
         return routineValidator.validate(persona, machine);
     }
