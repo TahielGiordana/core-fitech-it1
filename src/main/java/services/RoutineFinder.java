a4de2c4abcd9c62b9544f0129be5be723a8ce90e
Tony-L-93 <tonyliendro@gmail.com>
2023-04-11 00:46:48 -0300

    Se genera la lógica para buscar la rutina dada una persona

--- src/main/java/services/RoutineFinder.java
+++ src/main/java/services/RoutineFinder.java
@@ -3,11 +3,15 @@ package services;
 import pojos.Person;
 import pojos.Routine;
 
-import java.util.List;
+import java.util.Map;
 
 public class RoutineFinder {
 
-    public List<Routine> findRoutines(Person person){
-        return DatosDummy.getRutinasByPerson(person);
-    }
+	public Routine getRoutineByPerson(Person person) {
+		return RoutineMap.getRoutineMap().entrySet().stream()
+				.filter(entry -> entry.getKey().getName().equals(person.getName()) && entry.getKey().getLastname().equals(person.getLastname()))
+				.map(Map.Entry::getValue)
+				.findFirst()
+				.orElse(null);
+	}
 }
