e9b3ef6d205a0cd451eca4c21a6d1f20cfede922
Tony-L-93 <tonyliendro@gmail.com>
2023-04-13 00:18:10 -0300

    Se cambia el nombre de la carpeta y se pone ejemplo para probar el test

---
+++ src/test/java/criteriosAceptacionIteacion0/UserStory1.java
@@ -0,0 +1,16 @@
+package criteriosAceptacionIteacion0;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import pojos.Machine;
+import pojos.Person;
+
+public class UserStory1 {
+    static Machine stationaryBike;
+    static Person mateo;
+
+    @Test
+    public void testSomething(){
+        assertEquals(3+3,6);
+    }
+}
