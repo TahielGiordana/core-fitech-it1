304e784c10e6a616fa56275ad8a5f2d7cd9e8e0c
Tony-L-93 <tonyliendro@gmail.com>
2023-04-12 22:17:46 -0300

    Se agrega package test y se crea la clase UserStory1

---
+++ src/test/java/CriteriosAceptacionIteacion0/UserStory1.java
@@ -0,0 +1,16 @@
+package CriteriosAceptacionIteacion0;
+
+import pojos.Machine;
+import pojos.Person;
+
+//import org.junit.jupiter.api.BeforeAll;
+
+public class UserStory1 {
+    static Machine stationaryBike;
+    static Person mateo;
+
+//    @BeforeAll
+//    public static void iniciarEscenario(){
+//
+//    }
+}
