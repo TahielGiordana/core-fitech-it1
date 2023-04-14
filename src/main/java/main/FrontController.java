74c23c2ea482c9eb5408fc388b5c93b2b6092ede
TahielGiordana <tahielgiordana@gmail.com>
2023-04-14 00:18:14 -0300

    Muevo el frontController al package main para importarlo desde la ui

--- src/main/java/main/FrontController.java
+++ src/main/java/main/FrontController.java
@@ -1,3 +1,5 @@
+package main;
+
 import pojos.Machine;
 import pojos.Person;
 import services.RoutineFinder;
@@ -6,8 +8,10 @@ import validators.RoutineValidator;
 public class FrontController {
 
     public static void main(String[] args) {
-        Person person = new Person("Joan", "Ene");
-        Machine machine =new Machine("07569LK500IC0122", "Stationary Bike");
+        //Person person = new Person("Joan", "Ene");
+        Person person = new Person(args[0],args[1]);
+        //Machine machine =new Machine("07569LK500IC0122", "Stationary Bike");
+        Machine machine =new Machine(args[2], args[3]);
         FrontController frontController = new FrontController();
         boolean validateMachine = frontController.validateMachine(person, machine);
         System.out.println("Uso de Bicicleta Fija: " + validateMachine);
