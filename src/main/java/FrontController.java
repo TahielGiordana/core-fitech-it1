d0d51b77796c63a66dd7e3e438cea926bc8e1307
jferdev <fernando.ene@besysoft.com>
2023-04-15 17:50:06 -0300

    se quitan algunas carpetas

--- src/main/java/FrontController.java
+++ src/main/java/FrontController.java
@@ -6,10 +6,10 @@ import validators.RoutineValidator;
 public class FrontController {
 
     public static void main(String[] args) {
-        //Person person = new Person("Joan", "Ene");
-        Person person = new Person(args[0],args[1]);
-        //Machine machine =new Machine("07569LK500IC0122", "Stationary Bike");
-        Machine machine =new Machine(args[2], args[3]);
+        Person person = new Person("Joan", "Ene");
+        //Person person = new Person(args[0],args[1]);
+        Machine machine =new Machine("07569LK500IC0122", "Stationary Bike");
+        //Machine machine =new Machine(args[2], args[3]);
         FrontController frontController = new FrontController();
         boolean validateMachine = frontController.validateMachine(person, machine);
         System.out.println("Uso de Bicicleta Fija: " + validateMachine);
