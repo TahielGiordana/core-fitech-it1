c78abdf43ef47fb840ab974a9cae4d6bde102815
TahielGiordana <tahielgiordana@gmail.com>
2023-05-04 20:17:29 -0300

    Borro código del main

--- src/main/java/init/Core.java
+++ src/main/java/init/Core.java
@@ -1,8 +1,6 @@
 package init;
 
 import interfaces.Validator;
-import pojos.Machine;
-import pojos.Person;
 import services.ValidatorFinder;
 import services.ValidatorSequence;
 
@@ -20,9 +18,9 @@ public class Core{
     }
 
 
-    public boolean executeValidators(Person person, Machine machine){
-        System.out.println("inicio con datos: "+ person + "- maquina - " + machine);
-        return this.validatorSequence.executeValidators(person, machine);
+    public boolean executeValidators(String userName, String machineCode){
+        System.out.println("inicio con datos: "+ userName + "- maquina - " + machineCode);
+        return this.validatorSequence.executeValidators(userName, machineCode);
     }
 
 
@@ -30,34 +28,9 @@ public class Core{
 
         File file = new File("");
         System.out.println("path del file en el main" + file.getAbsolutePath());
-        Core core = new Core(file.getAbsolutePath() + "files");
-
-        /*Person person1 =new Person("persona_1"
-                , true
-                ,new Routine("ejercicio_1", new Machine("A2", "bicicleta_1")));
-        Machine machine1 =new Machine("A2", "bicicleta");
-
-
-        boolean b = core.executeValidators(person1,machine1);
-
-        System.out.println("resultado: " + b);*/
-
-        /*
-        *
-        File pluginDir = new File("plugins");
-        File[] pluginJars = pluginDir.listFiles((dir, name) -> name.endsWith(".jar"));
-
-        // Cargamos la clase del plugin dinámicamente usando una instancia de URLClassLoader
-        URLClassLoader classLoader = new URLClassLoader(new URL[]{pluginJars[0].toURI().toURL()});
-        Class<?> pluginClass = classLoader.loadClass("com.myplugin.MyPlugin");
-
-        // Instanciamos el plugin y ejecutamos su método
-        Plugin plugin = (Plugin) pluginClass.newInstance();
-        plugin.execute();
-        *
-        * */
+        Core core = new Core(file.getAbsolutePath() + "validators");
+        core.executeValidators("Tahiel", "BicicletaFija");
 
     }
-
 
 }
