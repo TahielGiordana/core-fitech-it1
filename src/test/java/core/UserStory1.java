7b29e83c2532c55d3e83a7e81992b79811b39b82
jferdev <fernando.ene@besysoft.com>
2023-05-08 22:04:43 -0300

    fix en test

--- src/test/java/core/UserStory1.java
+++ src/test/java/core/UserStory1.java
@@ -4,45 +4,37 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import services.ValidationEngine;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class UserStory1 {
 
     private Logger log = LogManager.getLogger("UserStory1");
-    /*private RoutineValidator validator;
-    private RoutineFinder routineFinder;
-    private Person joan;
+    Core core = new Core(new File("").getAbsolutePath());
+    ValidationEngine validationEngine = core.getValidatorManager();
 
-    private Person tahiel;
-
-    private Machine bike;*/
 
     @BeforeEach
     public void setUp() {
-        /*routineFinder = mock(RoutineFinder.class);
-        validator = new RoutineValidator(routineFinder);
-        bike = new Machine("A2","Bike");*/
-        log.warn("se ejecuta setUp de tests");
+
     }
 
     @Test
     public void CA1(){
-        /*Routine joanRoutine = new Routine("excersice 1", bike);
-        joan = new Person("persona_1",true,joanRoutine);
-        when(routineFinder.getRoutineByPerson(joan)).thenReturn(joanRoutine);
-        Boolean isValidMachine = validator.validate(joan, bike);
-        assertTrue(isValidMachine);*/
+        validationEngine.validate("Tahiel", "Bicicleta1");
+        assertTrue(validationEngine.getValidationResult());
         log.warn("se ejecuta test de CA1");
     }
 
     @Test
     public void CA2() {
-        /*Routine tahielRoutine = new Routine("excersice 2", new Machine("B2","Press"));;
-        tahiel = new Person("persona_2",true, tahielRoutine);
-        when(routineFinder.getRoutineByPerson(tahiel)).thenReturn(tahielRoutine);
-        Boolean isValidMachine = validator.validate(tahiel, bike);
-        assertFalse(isValidMachine);*/
-
+        validationEngine.validate("personaInv", "Bicicleta1");
+        assertFalse(validationEngine.getValidationResult());
         log.warn("se ejecuta test de CA2");
     }
 }
