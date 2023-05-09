3ea4803933ee2a571a938861520cb73d7b043946
AgustinMacias <agus_masus@outlook.com>
2023-05-08 22:26:33 -0300

    Fix test US1

--- src/test/java/core/UserStory1.java
+++ src/test/java/core/UserStory1.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import Core.Core;
 import services.ValidationEngine;
 
 import java.io.File;
