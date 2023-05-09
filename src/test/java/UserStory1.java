22cfe7d2b4971b80f4d5f09eb69e06a7cb2becba
AgustinMacias <agus_masus@outlook.com>
2023-05-08 21:00:28 -0300

    Modificado test US1

--- src/test/java/UserStory1.java
+++ src/test/java/UserStory1.java
@@ -1,48 +1,49 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import pojos.Machine;
-import pojos.Person;
-import pojos.Routine;
-import services.RoutineFinder;
-import validators.RoutineValidator;
+import Core.Core;
+import services.ValidationEngine;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import java.io.File;
 
 public class UserStory1 {
 
-    private RoutineValidator validator;
-    private RoutineFinder routineFinder;
-    private Person joan;
-
-    private Person tahiel;
-
-    private Machine bike;
+    Core core = new Core(new File("").getAbsolutePath());
+    ValidationEngine validationEngine = core.getValidatorManager();
 
     @BeforeEach
     public void setUp() {
-        routineFinder = mock(RoutineFinder.class);
-        validator = new RoutineValidator(routineFinder);
-        bike = new Machine("A2","Bike");
+        //routineFinder = mock(RoutineFinder.class);
+        //validator = new RoutineValidator(routineFinder);
+        //bike = new Machine("A2","Bike");
     }
 
     @Test
     public void CA1(){
+        /*
         Routine joanRoutine = new Routine("excersice 1", bike);
         joan = new Person("persona_1",true,joanRoutine);
         when(routineFinder.getRoutineByPerson(joan)).thenReturn(joanRoutine);
         Boolean isValidMachine = validator.validate(joan, bike);
         assertTrue(isValidMachine);
+        */
+        validationEngine.validate("Tahiel", "BicicletaFija");
+        assertTrue(validationEngine.getValidationResult());
     }
 
     @Test
     public void CA2() {
+        /*
         Routine tahielRoutine = new Routine("excersice 2", new Machine("B2","Press"));;
         tahiel = new Person("persona_2",true, tahielRoutine);
         when(routineFinder.getRoutineByPerson(tahiel)).thenReturn(tahielRoutine);
         Boolean isValidMachine = validator.validate(tahiel, bike);
         assertFalse(isValidMachine);
+        */
+        validationEngine.validate("personaInv", "BicicletaFija");
+        assertFalse(validationEngine.getValidationResult());
     }
 }
