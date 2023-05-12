0309467b24ba9e66aa91bbfe010c9071ebc2e13c
TahielGiordana <tahielgiordana@gmail.com>
2023-05-12 15:23:49 -0300

    Agrego subscribe al validationService

--- src/main/java/core/ValidationService.java
+++ src/main/java/core/ValidationService.java
@@ -1,5 +1,6 @@
 package core;
 
+import interfaces.Observer;
 import services.ValidatorFinder;
 import services.ObservableValidationEngine;
 
@@ -17,5 +18,9 @@ public class ValidationService {
 
     public boolean getResult(){
         return this.observableValidationEngine.getValidationResult();
+    }
+
+    public void subscribe(Observer observer){
+        this.observableValidationEngine.addObserver(observer);
     }
 }
