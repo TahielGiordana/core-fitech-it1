ca76c684e805bb02e2f867bdda06b336ee12bbf9
Tony-L-93 <tonyliendro@gmail.com>
2023-06-10 00:01:10 -0300

    Se borra codigo comentado

--- src/main/java/core/CoreFitech.java
+++ src/main/java/core/CoreFitech.java
@@ -16,30 +16,12 @@ import java.util.Set;
 public class CoreFitech{
     private final ValidationTask validationTask;
     private final ScoreTask scoreTask;
-    //private Map<String,Boolean> validationResult;
-
-    //private Set<String> checkedValidators;
 
     public CoreFitech(String validatorsPath, String machineCode, String scoresPath) throws FileNotFoundException {
-        //this.validationResult = new HashMap<>();
-        //this.checkedValidators = new HashSet<>();
-        //System.out.println("\u001B[31mCuando se inicia el Core isValid es: "+validationResult+"\u001B[0m");
         ValidatorFactory validatorFactory = new ValidatorFactory(validatorsPath, machineCode);
         this.validationTask = validatorFactory.buildValidationTask();
         this.scoreTask = new ScoreTask(scoresPath);
     }
-
-    /*public void checkValidator(String validatorName, boolean checked){
-        if(checked){
-            checkedValidators.add(validatorName);
-        }else{
-            checkedValidators.remove(validatorName);
-        }
-    }
-
-    public Map<String,Boolean> getResult(){
-        return this.validationResult;
-    }*/
 
     public ValidationTask getValidationTask() {
         return this.validationTask;
@@ -48,33 +30,4 @@ public class CoreFitech{
     public ScoreTask getScoreTask(){
         return this.scoreTask;
     }
-
-    /*@Override
-    public void update() {
-        Set<Validator> validators = validationTask.getValidators();
-        Map<String,Boolean> result = new HashMap<>();
-        for(Validator validator : validators){
-            if(checkedValidators.contains(validator.getClass().getName())){
-                if(validator.getResult() == null){
-                    return;
-                }else {
-                    if(!validator.getResult()){
-                        System.out.println("Falló el " + validator.getClass().getName());
-                    }else{
-                        System.out.println("Pasó el " + validator.getClass().getName());
-                    }
-                    result.put(validator.getClass().getName(),validator.getResult());
-                }
-            }else{
-                System.out.println(validator.getClass().getName()+" No está en la lista a checkear");
-            }
-        }
-        System.out.println("\u001B[31mEl resultado de Core era "+ validationResult + "y ahora es "+ result +"\u001B[0m");
-
-        if(validationResult.isEmpty() || !validationResult.equals(result)){
-            validationResult = result;
-            System.out.println("\u001B[31mAsi que notifico a la UI\u001B[0m");
-            notifyObservers();
-        }
-    }*/
 }
